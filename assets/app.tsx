8ff43731b30431e26f818e62b62a858db5d87ebb
Vincent Racelle <vracelle@positivethinking.lu>
2021-10-29 11:45:31 +0200

    remove stimulus

--- assets/app.tsx
+++ assets/app.tsx
@@ -5,11 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Home from "./Home";
