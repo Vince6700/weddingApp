07da9d9c72450318054f2861992993b8f4a5015d
Vincent Racelle <vracelle@positivethinking.lu>
2021-11-01 12:30:55 +0100

    validate form with formik

--- assets/Home.tsx
+++ assets/Home.tsx
@@ -1,42 +1,54 @@
 import React, { useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
 import useGuest from "./hooks/useGuest";
+import * as yup from "yup";
+import { useFormik } from "formik";
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email("Entrez une adresse mail valide")
+    .required("Email requis"),
+});
 
 const Home = () => {
-  const [email, setEmail] = useState<string>("");
   const {
     actions: { fetchGuest },
     error,
   } = useGuest();
 
-  const onClick = (e: any) => {
-    e.preventDefault();
-    fetchGuest(email);
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      fetchGuest(values.email);
+    },
+  });
 
   return (
     <Box>
       <Typography color="primary" sx={{ mb: 2 }}>
         Entre ton adresse email
       </Typography>
-      <Box component="form" autoComplete="off">
+      <Box component="form" autoComplete="off" onSubmit={formik.handleSubmit}>
         <TextField
           id="email"
           label="Email"
           variant="outlined"
-          type="email"
           name="email"
           fullWidth
           color="primary"
           sx={{ mb: 2 }}
-          value={email}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(event.currentTarget.value)
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={
+            (formik.touched.email && Boolean(formik.errors.email)) || !!error
           }
-          error={!!error}
-          helperText={error}
+          helperText={(formik.touched.email && formik.errors.email) || error}
         />
-        <Button variant="contained" onClick={onClick} fullWidth>
+        <Button variant="contained" type="submit" fullWidth>
           Voir mon invitation
         </Button>
       </Box>
