c67ea2ebdc155a56518411789642595c7a53fa23
Vincent Racelle <vracelle@positivethinking.lu>
2022-01-29 13:43:40 +0100

    fix media queries

--- assets/theme.ts
+++ assets/theme.ts
@@ -1,5 +1,19 @@
 import { ThemeOptions } from "@mui/material";
 
+declare module "@mui/material/styles" {
+  export interface BreakpointOverrides {
+    xs: false; // removes the `xs` breakpoint
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true; // adds the `mobile` breakpoint
+    tablet: true;
+    laptop: true;
+    desktop: true;
+  }
+}
+
 export const themeOptions: ThemeOptions = {
   palette: {
     primary: {
