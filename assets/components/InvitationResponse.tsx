d6d03697ad1ed764b53d5be6af777ebcee4f09cc
Vincent Racelle <vincentracelle@MacBook-Pro-de-Vince.local>
2022-02-20 17:28:49 +0100

    imporve and test site

--- assets/components/InvitationResponse.tsx
+++ assets/components/InvitationResponse.tsx
@@ -16,12 +16,18 @@ const InvitationResponse = ({ guest }: IInvitationResponse) => {
           backgroundColor: (theme: Theme) => `${theme.palette.primary.main}20`,
         }}
       />
-      <Typography color="primary">Votre réponse:</Typography>
-      <Typography color="primary">
-        {!!guest && guest?.adults > 0 && `${guest?.adults} adultes`}
+      <Typography color="primary" textAlign="center">
+        <strong>Votre réponse :</strong>
       </Typography>
       <Typography color="primary" textAlign="center">
-        {!!guest && guest?.children > 0 && `${guest?.children} enfants`}
+        {!!guest &&
+          guest?.adults > 0 &&
+          `${guest?.adults} ${guest?.adults === 1 ? "adulte" : "adultes"}`}
+      </Typography>
+      <Typography color="primary" textAlign="center">
+        {!!guest &&
+          guest?.children > 0 &&
+          `${guest?.children} ${guest?.children === 1 ? "enfant" : "enfants"}`}
       </Typography>
     </Box>
   );
